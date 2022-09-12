a488eaa12bf68c8397d8bd83dacecbcdc3dc14df
B3KALB <B3KALB@gmail.com>
2022-09-12 10:40:49 -0500

    tryme4

---
+++ tryme4.py
@@ -0,0 +1,21 @@
+import math
+# example 1
+def python_is_great(var):
+    incoming = str(var)
+    print(f"Working: {incoming}")
+    
+    python_is_great()  
+# example 2
+im_a_variable = "I'm a value"
+
+single_result = (math.ceil(9.94))
+im_an_expression = single_result
+
+python_is_great("I'm a value")
+python_is_great(im_a_variable)
+python_is_great(im_an_expression)
+# example 3
+
+# example 4
+
+# example 5
