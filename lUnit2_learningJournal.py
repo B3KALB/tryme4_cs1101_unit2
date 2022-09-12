a0afd23cfd29428e8263cfeeebd5b98d1f3bbbe1
B3KALB <B3KALB@gmail.com>
2022-09-12 14:54:42 -0500

    unit2LearningJournal
    Learning Journal Unit 2
    Welcome to the Unit 2 Learning Journal!
    
    Part 1
    The volume of a sphere is 4/3πr3, where π has the value of "pi" given in Section 2.1 of your textbook. Write a function called print_volume (r) that takes an argument for the sphere's radius and prints the sphere's volume.
    
    Call your print_volume function three times with different values for radius.
    
    Include all of the following in your Learning Journal:
    
    The code for your print_volume function.
    The inputs and outputs to three calls of your print_volume.
    
    Part 2
    Write your own function that illustrates a feature that you learned in this unit. The function must take at least one argument. The function should be your own creation, not copied from any other source. Do not copy a function from your textbook or the Internet.

--- lUnit2_learningJournal.py
+++ lUnit2_learningJournal.py
@@ -1,5 +1,6 @@
 from math import pi
 
+# PART 1
 def print_volume(r):
     volume_of_a_sphere = 4/3 * pi * r ** 3
     print(volume_of_a_sphere) 
@@ -10,3 +11,25 @@ print_volume(3)
 # output 113.09733552923254
 print_volume(5)
 # output 523.5987755982989
+
+
+# PART 2
+def part_two_fun(a, b):
+    make_more = a * 21
+    sell_it_all = b / 2
+    if make_more <= 63:
+        print("Make More now!")
+    elif make_more >= 250:
+        print("We have too much!")
+    else:
+        print("Just right!")
+    if sell_it_all <= 100:
+        print("Sell it all!!")
+    elif sell_it_all >= 3000:
+        print("Very nice!")
+    else:
+        print("Doing OK!")
+
+part_two_fun(2, 180)
+part_two_fun(13, 6600)
+part_two_fun(7, 3000)
