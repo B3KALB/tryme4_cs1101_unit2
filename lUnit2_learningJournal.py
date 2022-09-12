f0c9e9fb72e37cca2440df542efc164ad50cbe84
B3KALB <B3KALB@gmail.com>
2022-09-12 14:36:02 -0500

    Unit2LearningJournal
    Part 1
    The volume of a sphere is 4/3πr3, where π has the value of "pi" given in Section 2.1 of your textbook. Write a function called print_volume (r) that takes an argument for the sphere's radius and prints the sphere's volume.
    
    Call your print_volume function three times with different values for radius.

---
+++ lUnit2_learningJournal.py
@@ -0,0 +1,12 @@
+from math import pi
+
+def print_volume(r):
+    volume_of_a_sphere = 4/3 * pi * r ** 3
+    print(volume_of_a_sphere) 
+
+print_volume(1)
+# output 4.1887902047863905
+print_volume(3)
+# output 113.09733552923254
+print_volume(5)
+# output 523.5987755982989
